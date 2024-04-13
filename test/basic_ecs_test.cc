01707ccc30852cd81291ac220b49b8cf0ba0cd53
James Tateossian <jmtateossian50b@gmail.com>
2024-04-13 22:15:43 +0100

    removed fakeasset manager in test.

--- test/basic_ecs_test.cc
+++ test/basic_ecs_test.cc
@@ -10,19 +10,6 @@ using namespace ecstasy;
 using namespace ecstasy::component;
 using namespace ecstasy::systems;
 
-struct FakeAssetManager {
-  template<typename T>
-  struct Asset {
-    std::string path;
-    std::filesystem::file_time_type ftime;
-    T data;
-  };
-  std::unordered_map<std::string, Asset<Texture2D>> sprites;
-  std::unordered_map<std::string, Asset<std::vector<u8>>> scripts;
-};
-
-inline FakeAssetManager fa;
-
 TEST(scripting, adder) {
 
   EntityManager<20, CVelocity, CTransform2D> em; 
